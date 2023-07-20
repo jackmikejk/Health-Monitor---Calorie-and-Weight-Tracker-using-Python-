a859ec7e22a35245cdda24f8891a22c74ebf3d32
jackmikejk <116242574+jackmikejk@users.noreply.github.com>
2023-07-20 02:05:47 -0400

    Update the Random Choice Module

--- Weight_depicit_Visualize.py
+++ Weight_depicit_Visualize.py
@@ -1,31 +1,54 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-def main():
-    my_goal_weight = int(147)
-    print("Your Goal Weight is", my_goal_weight)
-    my_current_weight = float(input("What's your current weight today: "))
-    todays_calorie_intake = int(input("How much calories did you eat today: "))
+def plot_the_chart(current_weight_data, calorie_intake_data):
 
-    current_weight_data = []
-    calorie_intake_data = []
-    current_weight_data.append(my_current_weight)
-    calorie_intake_data.append(todays_calorie_intake)
+    plt.bar(current_weight_data, calorie_intake_data)
+    plt.xlabel("Current Weight")
+    plt.ylabel("Calorie Intake")
+    plt.title("Calorie Intake vs Current Weight")
+    plt.show()
 
-    if my_current_weight <= my_goal_weight:
-        print("Congrats, You've got your fitness back!")
+def weight_comparision(current_weight_data, goal_weight):
+    if goal_weight == current_weight_data:
+        print("You got this")
 
-def get_data_from_excel(filepath,sheet_name):
+def get_data_from_excel(filepath, sheet_name):
     try:
         # Read the Excel file into a pandas DataFrame
         xls = pd.ExcelFile(filepath)
         df = pd.read_excel(xls, sheet_name=sheet_name)
-        return df
+
+        # Filter the rows with non-empty "Calorie Intake" and "Current Weight" values
+        data = df.dropna(subset=["Calorie Intake", "Current Weight"])
+
+        # Get the "Calorie Intake" and "Current Weight" columns as lists
+        calorie_intake_data = data["Calorie Intake"].tolist()
+        current_weight_data = data["Current Weight"].tolist()
+
+        print("Calorie Intake Data:")
+        print(calorie_intake_data)
+
+        print("Current Weight Data:")
+        print(current_weight_data)
+        return current_weight_data, calorie_intake_data
     except FileNotFoundError:
         print(f"File '{filepath}' not found.")
     except Exception as e:
         print(f"Error: {e}")
 
+def main():
+    goal_weight = 147
+    print("Goal Weight:", goal_weight)
+    data_extraction = get_data_from_excel("D:\Coding\Python\Code practise\Simple Weight Depict Visualize\Weight_Depicit\Calorie_Tracker.xlsx", "Sheet1")
+    current_weight_data, calorie_intake_data = data_extraction
+
+    weight_comparision(current_weight_data, goal_weight)
+    plot_the_chart(current_weight_data, calorie_intake_data)
 
 if __name__ == "__main__":
     main()
-    data_extraction = get_data_from_excel("D:\Coding\Python\Code practise\Simple Weight Depict Visualize\Weight_Depicit\Calorie_Tracker.xlsx", "Sheet1")
+
+
+
